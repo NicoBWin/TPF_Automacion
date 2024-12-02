1ae15cd6a3c4a3e2481e759691c4b469a5c1d44f
matalvarez62275 <matalvarez@itba.edu.ar>
2024-12-02 15:59:04 -0300

    robotCreate(q0) y variables globales

--- TPF/main.m
+++ TPF/main.m
@@ -5,13 +5,25 @@
 % Ibañez, Lucía             - 62488
 
 close all; clear; clc
+%% Dimensiones Iniciales
+% Altura del lapiz
+pencil_height = 15;
+
+% Dimensiones de la hoja
+sheet_dimensions = [200, 150];
+
+% Vértice origen de la hoja (respecto a la base del Bichito)
+sheet_apex = [-100, 50, 0];
+
+% Posición Inicial
+q0 = deg2rad([90, 52.5, -75, -120, 0]);
 
 %% Obtener Imagen
-[name,path]=uigetfile({'*.png;*.jpg;*.jpeg'});
-Fname=strcat(path,name);
-clear path name
+% [name,path]=uigetfile({'*.png;*.jpg;*.jpeg'});
+% Fname=strcat(path,name);
+% clear path name
 
 %% Configuración del Robot
 
 % Creacion del Robot
-[manipulador] = robotCreate();
+[manipulador] = robotCreate(q0);
--- TPF/robotCreate.m
+++ TPF/robotCreate.m
@@ -1,13 +1,13 @@
-function [Bichito] = CreacionRobot()
+function [Bichito] = robotCreate(q0)
+%% Características del Bichito
 
-% Configuración inicial del robot
 % Número de links
 N = 5; 
 
 % Tipo de joint
 type = {'revolute', 'revolute', 'revolute', 'revolute', 'revolute'};
 
-% Largo de los links
+% Longitud de los links (consigna)
 L1 = 130;
 L2 = 144;
 L3 = 58;
@@ -26,10 +26,10 @@ I = {0*eye(3), 0*eye(3),  0*eye(3),  0*eye(3), 0*eye(3)};
 % Fricción unitaria
 B = [1, 1, 1, 1, 1]; 
 
-% Gear ratio
+% Gear ratio (distinto de 0)
 GR = [1, 1, 1, 1, 1];
 
-% Limites angulares
+% Límites angulares
 qlim = cell(1,N);
 qlim{1} = deg2rad([-180,180]);
 qlim{2} = deg2rad([-180,180]);
@@ -38,7 +38,7 @@ qlim{4} = deg2rad([-180,180]);
 qlim{5} = deg2rad([-180,180]);
 
 
-% Parametros DH
+% Parámetros DH
 DH = struct('d', cell(1,N), 'alpha', cell(1,N), 'a', cell(1,N), 'theta', cell(1,N));
 DH(1).a = 0; DH(1).alpha = 0; DH(1).d = L(1); 
 DH(2).a = 0; DH(2).alpha = pi/2; DH(2).d =0; 
@@ -46,30 +46,19 @@ DH(3).a = L(2); DH(3).alpha = 0; DH(3).d =0;
 DH(4).a = L(3); DH(4).alpha = 0; DH(4).d =0;
 DH(5).a = 0; DH(5).alpha = -pi/2; DH(5).d = L(4);
  
-%Creación de los links
+%% Creación de links
 % Arreglo de links
 for i = 1:N
     links{i} = Link(type{i}, 'modified', 'd', DH(i).d, 'a', DH(i).a, 'alpha', DH(i).alpha, ...
         'I', I{i}, 'm', M(i), 'B', B(i), 'qlim', qlim{i}, 'G', GR(i)); 
 end
 
-%Creación del robot
+%% Creación del Bichito
 
 Bichito = SerialLink([links{:}], 'tool', transl([0, 0, L(5)]), 'name', 'Bichito');
 
-%Posicion inicial manipulador
+% Posición inicial
 
-q0 = deg2rad([90, 52.5, -75, -120, 0]);
 figure()
 Bichito.teach(q0)
-
-% Altura del lapiz para realizar el movimiento
-Alt_lapiz = 15;
-
-% Dimensiones de la hoja
-Dim_h = [200, 150];
-
-%Posicion del vertice origen de la hoja respecto a la base
-Pos_h = [-100, 50, 0];
-
 end
