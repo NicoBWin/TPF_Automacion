600ef3d13e642add8ee3eb3115521228382f9336
Nicolás <nbustelow@gmail.com>
2024-12-02 23:59:05 -0300

    Update repo
    Add lineDetectorFunction and first approach to contorlPosition

--- TPF/controlPosition.m
+++ TPF/controlPosition.m
@@ -1,44 +1,96 @@
-function q = controlPosition(Bichito, q0, Pi, Pf, sheetDimensions, pencilHeight)
-%% Parametros del programa
-% Dimensiones de la hoja
-a = sheetDimensions(2);
-b = sheetDimensions(1);
+function q = controlPosition(bicho, radios, q0, Pi, Pf, sheetDimensions, pencilHeight)
+    %% Parametros del programa
+    % Dimensiones de la hoja
+    a = sheetDimensions(2);
+    b = sheetDimensions(1);
+    
+    % Area de trabajo
+    Rmin = radios(1);
+    Rmax = radios(2);
+    Rmedio = abs((radios(1) - radios(2))/2) + radios(1);
+    
+    % Numero de puntos intermedios a calcular en una trayectoria
+    N = 20;
 
-%% Transformación de puntos al sistema global ('0')
-% Se toman las coordenadas x e y, y se transforman considerando la posición
-% de la hoja respecto al manipulador
-% Notación:
-% - 0: Global
-% - h: Hoja
-% - i: Punto inicial de la recta
-% - f: Punto final de la recta
+    %% Dibujamos el cuadrado de trabajo
+    fh = figure('Name','Trayectoria');
+    bicho.plot(q0,'trail',{'r', 'LineWidth', 1,'LineStyle','--'}); 
+    rectangle('Position', [(-(Rmax - Rmin) / 2 - Rmin - a / 2) (-b / 2) a b], 'EdgeColor', 'b'); 
+    hold on
 
-xSheet = Pi(1);
-ySheet = Pi(2);
-piG(1) = -xSheet - Rmedio + a/2;
-piG(2) = ySheet - b/2;
-piG(3) = 0;
-piG = piG';
+    %% Transformación de puntos al sistema global del actuador
+    xSheet = Pi(1);
+    ySheet = Pi(2);
+    piG(1) = -xSheet - Rmedio + a/2;
+    piG(2) = ySheet - b/2;
+    piG(3) = 0;
+    piG = piG';
 
-xSheet = Pf(1);
-ySheet = Pf(2);
-pfG(1) = -xSheet - Rmedio + a/2;
-pfG(2) = ySheet - b/2;
-pfG(3) = 0;
-pfG = pfG';
+    xSheet = Pf(1);
+    ySheet = Pf(2);
+    pfG(1) = -xSheet - Rmedio + a/2;
+    pfG(2) = ySheet - b/2;
+    pfG(3) = 0;
+    pfG = pfG';
 
-%Matriz de rotación, indica la orientación del ee para escribir en la hoja 
-%respecto al sistema global
-Rh0 = [0 0 -1 
-        0 -1 0
-        -1 0 0];
+    % Matriz de rotación, indica la orientación del ee 
+    %para escribir en la hoja respecto al sistema global
+    Rh0 = [0 0 -1 
+           0 -1 0
+          -1 0 0];
 
-%Matriz de transformación del sist global al punto inicial de la línea
-Ti0 = [[Rh0' -Rh0' * piG]; [0 0 0 1]]; 
-T0i = inv(Ti0);
+    %Matriz de transformación del S.G. al punto inicial de la línea
+    Ti0 = [[Rh0' -Rh0' * piG]; [0 0 0 1]]; 
+    T0i = inv(Ti0);
 
-%Matriz de transformación del sist global al punto final de la línea
-Tf0 = [[Rh0' -Rh0' * pfG]; [0 0 0 1]]; 
-T0f = inv(Tf0);
+    %Matriz de transformación del S.G. al punto final de la línea
+    Tf0 = [[Rh0' -Rh0' * pfG]; [0 0 0 1]]; 
+    T0f = inv(Tf0);
+    
+    %% 1 - Posicionamiento del robot por encima de pi con pencilHeight
 
+    %Agrego el offset para acercar el ee
+    T0i_offset = T0i;
+    T0i_offset(3,4) = T0i_offset(3,4) + pencilHeight;
+
+    T_q0 = bicho.fkine(q0);
+    T0 = ctraj(T_q0,SE3(T0i_offset),N);
+    q{1} = bicho.ikine(T0, q0, 'mask', [1 1 1 0 1 1],'q0',q0);
+    bicho.plot(q{1},'trail',{'r', 'LineWidth', 1,'LineStyle','--'});
+
+
+    %% 2- Approach a la hoja (se baja pencilHeight)
+
+    T0 = ctraj(SE3(T0i_offset),SE3(T0i),5);
+    q{2} = bicho.ikine(T0, q{1}(end,:), 'mask', [1 1 1 0 1 1],'q0',q{1}(end,:));
+    bicho.plot(q{2},'trail',{'r', 'LineWidth', 1,'LineStyle','--'});
+
+    %% 3- Dibujo la recta que une los puntos ingresados 
+
+    T1 = ctraj(SE3(T0i),SE3(T0f),N);
+    q{3} = bicho.ikine(T1, q{2}(end,:), 'mask', [1 1 1 0 1 1],'q0',q{2}(end,:));
+    bicho.plot(q{3},'trail',{'b', 'LineWidth', 2});
+
+    translVecX = zeros(size(T1)); translVecY = zeros(size(T1)); translVecZ = zeros(size(T1));
+    for i = 1:size(T1,2)
+        aux = T1(i).double;
+        translVecX(i) = aux(1,end);
+        translVecY(i) = aux(2,end);
+        translVecZ(i) = aux(3,end);
+    end
+    plot3(translVecX,translVecY,translVecZ,'-r');
+    hold on
+
+    %% 4- Retiramos de la hoja (subo pencilHeight)
+    T0f_offset = T0f;
+    T0f_offset(3,4) = T0f_offset(3,4) + pencilHeight;
+
+    T2 = ctraj(SE3(T0f),SE3(T0f_offset),5);
+    q{4} = bicho.ikine(T2, q{3}(end,:), 'mask', [1 1 1 0 1 1],'q0',q{3}(end,:));
+    bicho.plot(q{4},'trail',{'r', 'LineWidth', 1,'LineStyle','--'});
+
+    %% 5- Vuelvo a la posicion original
+    T3 = ctraj(SE3(T0f_offset),T_q0,N);
+    q{5} = bicho.ikine(T3, q{4}(end,:), 'mask', [1 1 1 0 1 1],'q0',q{4}(end,:));
+    bicho.plot(q{5},'trail',{'r', 'LineWidth', 1,'LineStyle','--'});
 end
--- TPF/main.m
+++ TPF/main.m
@@ -1,4 +1,6 @@
-%% TP Final Automación Industrial
+%% TP Final Automación Industrial 
+% Diciembre 2024
+%
 % Alvarez, Matías Ezequiel  - 62275
 % Bustelo, Nicolás          - 61431
 % Galán, Albertina          - 61665
@@ -7,29 +9,27 @@
 close all; clear; clc
 %% Dimensiones Iniciales
 % Altura del lapiz
-pencilHeight = 15;
-
+pencilHeight = 10;
 % Dimensiones de la hoja
 sheetDimensions = [150, 200];
-
 % Vértice origen de la hoja (respecto a la base del Bichito)
 sheetApex = [-100, 50, 0];
-
 % Posición Inicial
 q0 = deg2rad([90, 52.5, -75, -120, 0]);
 
 %% Obtener Imagen
 % [name,path]=uigetfile({'*.png;*.jpg;*.jpeg'});
-% Fname=strcat(path,name);
+% fileName=strcat(path,name);
 % clear path name
 
-%% Configuración del Robot
-
-% Creacion del Robot
+%% Configuración y creación del Robot
 [Bichito] = robotCreate(q0);
 
 %% Espacio de trabajo 
 [rMax,rMin] = workSpace(Bichito, q0, sheetDimensions);
 
+%% Análisis de imagén
+% limitCoords = lineDetector(fileName);
+
 %% Trayectoria
-% qTrayectoria = controlPosition(Bichito, q0, pts(1,:), pts(2,:),sheetDimensions, pencilHeight);
+% movimiento = controlPosition(Bichito, q0, pts(1,:), pts(2,:),sheetDimensions, pencilHeight);
